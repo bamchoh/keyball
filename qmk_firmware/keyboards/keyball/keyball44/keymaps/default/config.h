3a5f0176e250f0b34149be5f3c52dc8b27fada01
bamchoh <bamchoh@gmail.com>
2024-04-03 23:56:46 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/default/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/default/config.h
@@ -35,5 +35,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
+#ifdef SPLIT_USB_DETECT
+# undef SPLIT_USB_DETECT 
+#endif
+
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
